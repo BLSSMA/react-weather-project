bb07b759b4f6ce8be111da9fc226a2f4bda0e1c0
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-09-01 11:17:07 -0400

    made skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
-  return (
-    <div className="App">
-<h1> this is a weather app</h1>
+  return (    
+  <div className="App">
+    <div className="container"><Weather /></div>
+
+<div col-6>This App Was Coded by Blossom Armstrong and is Open-Sourced on <a href ="https://github.com/BLSSMA/react-weather-project" target="_blank" rel="noreferrer">GitHub</a></div>
     </div>
   );
 }
-
 
