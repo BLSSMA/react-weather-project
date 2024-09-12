c9d8423739d9f3eca7f8690acf0ebe23271ded25
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-09-12 15:50:55 -0400

    finalized footer links

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   <div className="App">
     <div className="container"><Weather defaultCity="New York"/></div>
 
-<div className="footer">This App Was Coded by Blossom Armstrong and is Open-Sourced on <a href ="https://github.com/BLSSMA/react-weather-project" target="_blank" rel="noreferrer">GitHub</a></div>
+<div className="footer">This App Was Coded by <a href="https://www.shecodes.io/graduates/123692-blossom-armstrong" target="_blank" rel="noreferrer">Blossom Armstrong</a>, is hosted on <a href="https://app.netlify.com/sites/react-weather-project-ba/deploys" target="_blank" rel="noreferrer">Netlify</a> and is Open-Sourced on <a href ="https://github.com/BLSSMA/react-weather-project" target="_blank" rel="noreferrer">GitHub</a></div>
     </div>
   );
 }
