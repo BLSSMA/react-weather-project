66b98c5a63041b1600fa44c444c5d08cddd26ba7
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-09-12 11:03:17 -0400

    updated codeMapping

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,33 +1,33 @@
 import React from "react";
+
 import ReactAnimatedWeather from 'react-animated-weather';
 
 
 export default function WeatherIcon(props){
 const codeMapping = {
-    "clear-sky-day": "clear-day",
-    "clear-sky-night": "clear-night",
-    "few-clouds-day": "partly-cloudy-day",
-    "few-clouds-night": "partly-cloudy-night",
-    "scattered-clouds-day": "cloudy",
-    "scattered-clouds-night": "cloudy",
-    "broken-clouds-day": "fog",
-    "broken-clouds-night": "fog",
-    "shower-rain-day": "rain",
-    "shower-rain-night": "rain",
-    "rain-day": "rain",
-    "rain-night": "rain",
-    "thunderstorm-day": "rain",
-    "thunderstorm-night": "rain",
-    "snow-day": "snow",
-    "snow-night": "snow",
-    "mist-day": "wind",
-    "mist-night": "wind"
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "CLOUDY",
+    "scattered-clouds-night": "CLOUDY",
+    "broken-clouds-day": "FOG",
+    "broken-clouds-night": "FOG",
+    "shower-rain-day": "RAIN",
+    "shower-rain-night": "RAIN",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "thunderstorm-day": "RAIN",
+    "thunderstorm-night": "RAIN",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "WIND",
+    "mist-night": "WIND"
 };
 return( 
     <ReactAnimatedWeather
-    icon={codeMapping[props.code]} 
-    color={black}
-    size={512}
+    icon={codeMapping[props.code[0]]} 
+    size={300}
     animate={true}
   />
 
