3512de4b8f1fab169851c0b0ba276447b7811170
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-09-17 08:38:20 -0400

    working on new component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -24,7 +24,7 @@ let [forecast, setDailyForecast] = useState(null);
                             <span className="forecastIcon d-flex-grid col-3 space-evenly">
                                 <WeatherIcon code={forecast[0].condition.icon} size={40} color="black"/>
                                 </span>
-                            <span className="forecastDay d-flex-grid col-3 space-evenly">{<ForecastDateFormat/>*1000}</span>
+                            <span className="forecastDay d-flex-grid col-3 space-evenly"><ForecastDateFormat data={forecast[0].time}/></span>
                             <span className="forecastHigh d-flex-grid col-3 space-evenly">{Math.round(forecast[0].temperature.maximum)}℃</span>
                             <span className="forecastLow d-flex-grid col-3 space-evenly">{Math.round(forecast[0].temperature.minimum)}℃</span>
                             </div>
@@ -32,7 +32,8 @@ let [forecast, setDailyForecast] = useState(null);
                    <hr />
                 </ul>
 </div>);
-}else{    let key = `01dd2bca25c0t00b3d253f443e0of791`;
+}else{    
+    let key = `01dd2bca25c0t00b3d253f443e0of791`;
     let latitude = props.coordinates.latitude;
     let longitude = props.coordinates.longitude;
     let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${key}&unit=metric
